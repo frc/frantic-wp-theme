62aa0fa08eab10ec37cc08ea9a6aa7006bcf3b6b
Tuomas Arokanto <tuomas.arokanto@frantic.com>
2013-06-10 14:39:12 +0300

    separated custom post types and taxonomies into a separate file from functions.php.

--- functions.php
+++ functions.php
@@ -4,6 +4,7 @@
 require_once('lib/cleanup.php'); 
 require_once('lib/scripts_and_styles.php'); 
 require_once('lib/disable_pingback.php');
+require_once('lib/post_types_and_taxonomies.php');
 
 
 // add RSS links to <head> section
@@ -18,65 +19,7 @@ add_theme_support('menus');
 add_theme_support('automatic-feed-links');
 
 
-// new taxonomy
 
-/*function cases_init() {
-	// create a new taxonomy
-	register_taxonomy(
-		'cases',
-		'works',
-		array(
-			'hierarchical' => true,
-			'label' => __('Cases'),
-			'sort' => true,
-			'args' => array('orderby' => 'term_order'),
-			'rewrite' => array('slug' => 'cases')
-		)
-	);
-}
-add_action( 'init', 'cases_init' );
-*/
-
-// custom post type
-
-/*
-add_action('init', 'create_post_type');
-
-function create_post_type() {
-	register_post_type( 'custom-post-type',
-		array(
-			'labels' => array(
-				'name' => _x('custom-post-type', 'post type general name'),
-				'singular_name' => _x('custom-post-type', 'post type singular name'),
-			    'add_new' => _x('Add new', 'custom-post-type'),
-			    'add_new_item' => __('Add new custom-post-type'),
-		    	'edit_item' => __('Edit item'),
-    			'new_item' => __('New custom-post-type'),
-			    'all_items' => __('All custom-post-type'),
-    			'view_item' => __('Show custom-post-type'),
-			    'search_items' => __('Find custom-post-type'),
-		    	'not_found' =>  __('Not found'),
-	    		'not_found_in_trash' => __('Nothing in trash'), 
-    			'parent_item_colon' => '',
-			    'menu_name' => 'custom-post-type'
-			),
-			'public' => true,
-		    'publicly_queryable' => true,
-    		'show_ui' => true, 
-		    'show_in_menu' => true, 
-    		'query_var' => true,
-			//'rewrite' => array('slug' => 'something'),
-	    	'capability_type' => 'post',
-			'has_archive' => true,
-    		'hierarchical' => false,
-			'menu_position' => 5,
-			'supports' => array('title', 'editor', 'excerpt', 'custom-fields', 'author', 'thumbnail', 'comments'),
-			'taxonomies' => array('post_tag','category')
-		)
-	);
-		
-}
-*/
 
 
 // add excerpt to pages
---
+++ lib/post_types_and_taxonomies.php
@@ -0,0 +1,73 @@
+<?php
+
+/* 
+ * Add all your Custom Post Types and Custom Taxonomies here.
+ * You might want to use an online tool in http://generatewp.com/post-type/ to 
+ * create Custom Post Types scripts.
+*/
+
+
+
+// new taxonomy
+
+/*function cases_init() {
+	// create a new taxonomy
+	register_taxonomy(
+		'cases',
+		'works',
+		array(
+			'hierarchical' => true,
+			'label' => __('Cases'),
+			'sort' => true,
+			'args' => array('orderby' => 'term_order'),
+			'rewrite' => array('slug' => 'cases')
+		)
+	);
+}
+add_action( 'init', 'cases_init' );
+*/
+
+
+// custom post type
+
+/*
+add_action('init', 'create_post_type');
+
+function create_post_type() {
+	register_post_type( 'custom-post-type',
+		array(
+			'labels' => array(
+				'name' => _x('custom-post-type', 'post type general name'),
+				'singular_name' => _x('custom-post-type', 'post type singular name'),
+			    'add_new' => _x('Add new', 'custom-post-type'),
+			    'add_new_item' => __('Add new custom-post-type'),
+		    	'edit_item' => __('Edit item'),
+    			'new_item' => __('New custom-post-type'),
+			    'all_items' => __('All custom-post-type'),
+    			'view_item' => __('Show custom-post-type'),
+			    'search_items' => __('Find custom-post-type'),
+		    	'not_found' =>  __('Not found'),
+	    		'not_found_in_trash' => __('Nothing in trash'), 
+    			'parent_item_colon' => '',
+			    'menu_name' => 'custom-post-type'
+			),
+			'public' => true,
+		    'publicly_queryable' => true,
+    		'show_ui' => true, 
+		    'show_in_menu' => true, 
+    		'query_var' => true,
+			//'rewrite' => array('slug' => 'something'),
+	    	'capability_type' => 'post',
+			'has_archive' => true,
+    		'hierarchical' => false,
+			'menu_position' => 5,
+			'supports' => array('title', 'editor', 'excerpt', 'custom-fields', 'author', 'thumbnail', 'comments'),
+			'taxonomies' => array('post_tag','category')
+		)
+	);
+		
+}
+*/
+
+*/
+?>
