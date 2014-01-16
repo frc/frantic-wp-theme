bf983b3a16163b116b6133f406bf0b3d0b74ce24
Antti Onttonen <antti.onttonen@frantic.com>
2014-01-16 10:42:54 +0200

    Cleaned up some template files

--- functions.php
+++ functions.php
@@ -1,109 +1,59 @@
-<?php	
+<?php
 
-// Stuff in external files
+/**
+ * External libs for cleanup, registerings scripts, etc.
+ */
 require_once('lib/cleanup.php'); 
-require_once('lib/scripts_and_styles.php'); 
+require_once('lib/scripts.php'); 
 require_once('lib/disable_pingback.php');
 require_once('lib/post_types_and_taxonomies.php');
-//require_once('lib/navigation.php');
+// require_once('lib/navigation.php');
 
 
-// add RSS links to <head> section
-
-add_theme_support('automatic_feed_links');
-
-// thumbnail & menu support
-
-
-add_theme_support('post-thumbnails');
-add_theme_support('menus');
+/**
+ * Theme feature(s) support.
+ */
 add_theme_support('automatic-feed-links');
+add_theme_support('menus');
+add_theme_support('post-thumbnails');
+
+// add_post_type_support('page', 'excerpt');
 
 
-
-
-
-// add excerpt to pages
-
-//add_post_type_support('page', 'excerpt');
-
-
-
-
-// register navigation
-
+/**
+ * Register menu(s).
+ */
 register_nav_menu('main', 'main');
 
 
-// Reorder admin menu
-function custom_menu_order($menu_ord) {  
-    if (!$menu_ord) return true;  
-    
-    return array(  
-        'index.php', // Dashboard  
-        'separator1', // First separator  
-        'edit.php?post_type=page', // Pages  
-        'edit.php', // Posts  
-        // Add custom post types here like this:
-        // 'edit.php?post_type=event',
-        // Gravity forms:
-        // 'admin.php?page=gf_edit_forms',
-        'upload.php', // Media  
-        'edit-comments.php', // Comments  
-        'separator2', // Second separator  
-        'themes.php', // Appearance  
-        'plugins.php', // Plugins  
-        'users.php', // Users  
-        'tools.php', // Tools  
-        'options-general.php', // Settings  
-        'separator-last', // Last separator  
-    );  
-}  
-add_filter('custom_menu_order', 'custom_menu_order'); // Activate custom_menu_order  
-add_filter('menu_order', 'custom_menu_order'); 
+/**
+ * Reorder admin menu.
+ */
+function custom_menu_order($menu_ord) {
+	if (!$menu_ord) return true;
 
-// Custom thumbnails
-/*
-if (function_exists('add_image_size')){ 
-	//add_image_size('gallery', 300, 200, true);
-	//add_image_size( 'masonry-img', 320, 9999 ); 
+	return array(
+		'index.php', // Dashboard
+		'separator1', // First separator
+		'edit.php?post_type=page', // Pages
+		'edit.php', // Posts
+		// Add custom post types here like this:
+		// 'edit.php?post_type=event',
+		// Gravity forms:
+		// 'admin.php?page=gf_edit_forms',
+		'upload.php', // Media
+		'edit-comments.php', // Comments
+		'separator2', // Second separator
+		'themes.php', // Appearance
+		'plugins.php', // Plugins
+		'users.php', // Users
+		'tools.php', // Tools
+		'options-general.php', // Settings
+		'separator-last', // Last separator
+	);
 }
-*/
-
-// remove links emnu
-/*
-function remove_menu_items() {
-  global $menu;
-  $restricted = array(__('Links'), __('Comments'));
-  end ($menu);
-  while (prev($menu)){
-    $value = explode(' ',$menu[key($menu)][0]);
-    if(in_array($value[0] != NULL?$value[0]:"" , $restricted)){
-      unset($menu[key($menu)]);}
-    }
-  }
-
-add_action('admin_menu', 'remove_menu_items');
-
-// detect browser (if for some reason you need a special hack)
-
-/*
-add_filter('body_class','browser_body_class');
-function browser_body_class($classes) {
-	global $is_lynx, $is_gecko, $is_IE, $is_opera, $is_NS4, $is_safari, $is_chrome, $is_iphone;
-	if($is_lynx) $classes[] = 'lynx';
-	elseif($is_gecko) $classes[] = 'mozilla';
-	elseif($is_opera) $classes[] = 'opera';
-	elseif($is_NS4) $classes[] = 'ns4';
-	elseif($is_safari) $classes[] = 'safari';
-	elseif($is_chrome) $classes[] = 'chrome';
-	elseif($is_IE) $classes[] = 'ie';
-	else $classes[] = 'browser-unknown';
-	if($is_iphone) $classes[] = 'iphone';
-	return $classes;
-}
-*/
-
+add_filter('custom_menu_order', 'custom_menu_order'); // Activate custom_menu_order
+add_filter('menu_order', 'custom_menu_order');
 
 
 ?>
--- header.php
+++ header.php
@@ -1,70 +1,53 @@
-<!doctype html>
-
-<!-- HTML5 Boilerplate -->
+<!DOCTYPE html>
 <!--[if lt IE 7]><html class="no-js lt-ie9 lt-ie8 lt-ie7" <?php language_attributes(); ?>> <![endif]-->
 <!--[if (IE 7)&!(IEMobile)]><html class="no-js lt-ie9 lt-ie8" <?php language_attributes(); ?>><![endif]-->
 <!--[if (IE 8)&!(IEMobile)]><html class="no-js lt-ie9" <?php language_attributes(); ?>><![endif]-->
 <!--[if gt IE 8]><!--> <html class="no-js" <?php language_attributes(); ?>><!--<![endif]-->
-
 <head>
+	<meta charset="<?php bloginfo('charset'); ?>">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title><?php bloginfo('name'); ?></title>
+	<meta name="description" content="">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 
-<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta property="og:description" content="">
+	<meta property="og:image" content="">
+	<meta property="og:title" content="<?php bloginfo('name'); ?>">
+	<meta property="og:type" content="website">
+	<meta property="og:url" content="">
 
-<title><?php bloginfo( 'name' ) ?></title>
+	<!--[if gt IE 8]><!-->
+	<link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/css/style.css">
+	<!--<![endif]-->
 
-<meta name="HandheldFriendly" content="True">
-<meta name="MobileOptimized" content="320">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<!--[if lte IE 8]>
+	<link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/css/ie.css">
+	<![endif]-->
 
-<meta property="og:title" content="<?php bloginfo( 'name' ) ?>">
-<meta property="og:description" content="">
-<meta property="og:type" content="website">
-<meta property="og:url" content="http://.com">
-<meta property="og:image" content="">
-<meta property="fb:admins" content="">
+	<!--[if (lt IE 9) & (!IEMobile)]>
+	<script src="<?php echo get_stylesheet_directory_uri(); ?>/js/vendor/ie/nwmatcher-min.js"></script>
+	<script src="<?php echo get_stylesheet_directory_uri(); ?>/js/vendor/ie/selectivizr-min.js"></script>
+	<![endif]-->
 
+	<!-- ICONS -->
+	<link rel="shortcut icon" href="/favicon.ico">
+	<link rel="shortcut icon" href="/favicon.png">
 
-<!--[if gt IE 8]><!-->
-<link rel="stylesheet" href="<?php bloginfo( 'template_directory' ); ?>/css/style.css">
-<!--<![endif]-->
-	
-<!--[if lte IE 8]>
-<link rel="stylesheet" href="<?php bloginfo( 'template_directory' ); ?>/css/ie.css">
-<![endif]-->
+	<?php // if ( is_singular() ) wp_enqueue_script( 'comment-reply' ); ?>
 
-<!-- scripts -->
-<script src="<?php bloginfo( 'template_directory' ); ?>/js/vendor/modernizr-2.5.3-min.js"></script>
-
-<!--[if (lt IE 9) & (!IEMobile)]>
-<script src="<?php bloginfo( 'template_directory' ); ?>/js/vendor/ie/nwmatcher-min.js"></script>
-<script src="<?php bloginfo( 'template_directory' ); ?>/js/vendor/ie/selectivizr-min.js"></script>
-<![endif]-->
-
-
-<!-- ICONS -->
-<link rel="shortcut icon" href="/favicon.ico">
-<link rel="shortcut icon" href="/favicon.png">
-
-<link rel="canonical" href="/">
-
-<?php //if ( is_singular() ) wp_enqueue_script( 'comment-reply' ); ?>
-
-<?php wp_head(); ?>
-
+	<?php wp_head(); ?>
 </head>
-
 <body <?php body_class(); ?>>
 
 <div class="l-wrap">
-	
+
 	<div class="l-constrained">
 
 		<header role="banner">
-			<h1>Framework</h1>
-		</header><!-- end banner -->
+			<h1><?php bloginfo('name'); ?></h1>
+		</header>
 
 		<nav role="navigation">
-		
 			<ul class="nav-primary">
 			<?php wp_nav_menu(
 				array(
@@ -74,8 +57,6 @@
 				)
 			); ?>
 			</ul>
-		
-		</nav><!-- end navigation -->
+		</nav>
 
-	</div><!-- end content -->
-
+	</div>
