63e6a28b3b287c16f1c5c45e6ce35221a450f1ea
Antti Huovinen <antti.huovinen@frantic.com>
2015-05-25 12:01:06 +0300

    Cleanup: Remove emojis.

--- lib/cleanup.php
+++ lib/cleanup.php
@@ -3,6 +3,15 @@
 /**
  * Cleanup ’head’
  */
+
+function disable_emojicons_tinymce( $plugins ) {
+    if ( is_array( $plugins ) ) {
+        return array_diff( $plugins, array( 'wpemoji' ) );
+    } else {
+        return array();
+    }
+}
+
 function cleanup() {
     remove_action('wp_head', 'adjacent_posts_rel_link', 10, 0);
     remove_action('wp_head', 'feed_links', 2);
@@ -14,6 +23,18 @@ function cleanup() {
     remove_action('wp_head', 'wlwmanifest_link');
 
     //wp_deregister_script('jquery');
+
+    // all actions related to emojis
+    remove_action( 'admin_print_styles', 'print_emoji_styles' );
+    remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
+    remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
+    remove_action( 'wp_print_styles', 'print_emoji_styles' );
+    remove_filter( 'wp_mail', 'wp_staticize_emoji_for_email' );
+    remove_filter( 'the_content_feed', 'wp_staticize_emoji' );
+    remove_filter( 'comment_text_rss', 'wp_staticize_emoji' );
+
+    // filter to remove TinyMCE emojis
+    add_filter( 'tiny_mce_plugins', 'disable_emojicons_tinymce' );
 }
 add_action('init', 'cleanup');
 
