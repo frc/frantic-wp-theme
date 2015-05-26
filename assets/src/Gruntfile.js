02481702a7264c7dd173afaec891dae4e839b8a0
Niko Pettersen <niko.pettersen@frantic.com>
2015-05-26 16:14:55 +0300

    Theme: Add new Grunt tasks

--- assets/src/Gruntfile.js
+++ assets/src/Gruntfile.js
@@ -1,96 +1,41 @@
-module.exports = function(grunt) {
+module.exports = function ( grunt ) {
 
-    // Project configuration.
-    grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
+	'use strict';
 
-        copy: {
-            dist: {
-                files: [{
-                    src: ['js/vendor/**'],
-                    dest: '../'
-                }]
-            }
-        },
+/**
+ * Tasks and configuration
+ *
+ * @link https://github.com/firstandthird/load-grunt-config
+ */
 
-        sass: {
-            dist: {
-                options: {
-                    //style: 'compressed',
-                    precision: 10
-                },
-                files: {
-                    '../css/main.css': 'scss/style.scss',
-                    '../css/ie.css': 'scss/ie.scss'
-                }
-            },
-            /*dev: {
-                options: {
-                    precision: 10
-                },
-                files: {
-                    '../css/main.css': 'scss/style.scss',
-                    '../css/ie.css': 'scss/ie.scss'
-                }
-            }*/
-        },
+	require('load-grunt-config')(grunt, {
 
-        autoprefixer: {
-            options: {
-                browsers: ['last 2 version', 'ie 8', 'ie 9']
-            },
-            multiple_files: {
-                expand: true,
-                flatten: true,
-                src: '../css/*.css',
-                dest: '../css/'
-            },
-        },
+		configPath: require('path').join( process.cwd(), 'grunt-tasks' ),
 
-        cssmin: {
-            files: {
-                '../css/min/main.min.css': ['../css/main.css'],
-                '../css/min/ie.min.css': ['../css/ie.css']
-            }
-        },
+		init: true,
 
-        uglify: {
-            dist: {
-                options: {
-                    compress: true
-                },
-                files: {
-                    '../js/main.min.js': 'js/*.js'
-                }
-            },
-            dev: {
-                options: {
-                    mangle: false,
-                    compress: false,
-                    beautify: true
-                },
-                files: {
-                    '../js/main.min.js': 'js/*.js'
-                }
-            }
-        },
+		data: {
 
-        watch: {
-            files: ['scss/**/*', 'js/*.js'],
-            tasks: 'default'
-        }
-    });
+			// Load themes
+			config: grunt.file.readJSON('theme.json'),
 
-    // Load plugin(s) needed for task(s)
-    grunt.loadNpmTasks('grunt-contrib-copy');
-    grunt.loadNpmTasks('grunt-contrib-sass');
-    grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-contrib-cssmin');
-    grunt.loadNpmTasks('grunt-autoprefixer');
+		},
 
-    // Register task(s)
-    grunt.registerTask('default', ['copy:dist', 'sass:dist', 'autoprefixer', 'cssmin', 'uglify:dist']);
-    grunt.registerTask('dev', ['sass:dev', 'uglify:dev']);
+		// Can optionally pass options to load-grunt-tasks.
+		// If you set to false, it will disable auto loading tasks.
+		loadGruntTasks: {
+			pattern: 'grunt-*',
+			config: require('./package.json'),
+			scope: 'devDependencies'
+		},
+
+		// Can post process config object before it gets passed to grunt
+		postProcess: function( config ) {},
+
+		// Allows to manipulate the config object before it gets merged with the data object
+		preMerge: function( config, data ) {}
+	});
+
+	require('time-grunt')( grunt );
 
 };
---
+++ assets/src/grunt-tasks/autoprefixer.js
@@ -0,0 +1,54 @@
+/**
+ * Parses CSS and adds vendor-prefixed CSS properties using the Can I Use database
+ * @link https://github.com/nDmitry/grunt-autoprefixer
+ */
+
+	module.exports = {
+
+		admin: {
+			options: {
+				browsers: ['<%= config.theme.browser_support %>'],
+				cascade: true,
+				safe: true
+			},
+			src: '<%= config.theme.assets.css.location %>/<%= config.theme.assets.css.admin %>'
+		},
+
+		editor: {
+			options: {
+				browsers: ['<%= config.theme.browser_support %>'],
+				cascade: true,
+				safe: true
+			},
+			src: '<%= config.theme.assets.css.location %>/<%= config.theme.assets.css.editor %>'
+		},
+
+		login: {
+			options: {
+				browsers: ['<%= config.theme.browser_support %>'],
+				cascade: true,
+				safe: true
+			},
+			src: '<%= config.theme.assets.css.location %>/<%= config.theme.assets.css.login %>'
+		},
+
+		main: {
+			options: {
+				browsers: ['<%= config.theme.browser_support %>'],
+				cascade: true
+			},
+			files: {
+				'<%= config.theme.assets.css.location %>/<%= config.theme.assets.css.main %>': '<%= config.theme.assets.css.location %>/<%= config.theme.assets.css.main %>',
+				'<%= config.theme.assets.css.location %>/<%= config.theme.assets.css.main_min %>': '<%= config.theme.assets.css.location %>/<%= config.theme.assets.css.main_min %>',
+			}
+		},
+
+		print: {
+			options: {
+				browsers: ['<%= config.theme.browser_support %>'],
+				cascade: true
+			},
+			src: '<%= config.theme.assets.css.location %>/<%= config.theme.assets.css.print %>'
+		}
+
+	};
