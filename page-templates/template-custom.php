71382af5e9984e7170312e0e66f137d2edd17c1c
Niko Pettersen <niko.pettersen@frantic.com>
2015-05-26 16:16:31 +0300

    Theme: Add new WordPress templates

--- page-templates/template-custom.php
+++ page-templates/template-custom.php
@@ -1,11 +1,6 @@
 <?php
 /**
- * Template for displaying all pages
- *
- * This is the template that displays all pages by default.
- * Please note that this is the WordPress construct of pages
- * and that other 'pages' on your WordPress site will use a
- * different template.
+ * Template Name: Custom template
  *
  * @package _frc
  */
@@ -17,7 +12,7 @@
 
 			<?php while ( have_posts() ) : the_post();
 
-				get_template_part( 'template-parts/content', 'page' );
+				get_template_part( 'template-parts/content', 'single' );
 
 				// if ( comments_open() || get_comments_number() ) comments_template();
 
