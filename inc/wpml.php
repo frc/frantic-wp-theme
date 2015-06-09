b1f75314d6f95a06f53ea8e991ff0f92ec9e7c02
Niko Pettersen <niko.pettersen@frantic.com>
2015-06-09 11:31:18 +0300

    Theme: Remove WPML generator tag

--- inc/wpml.php
+++ inc/wpml.php
@@ -5,46 +5,46 @@
  * @package _frc
  */
 
-/**
- * Remove general WPML features
- */
-
 add_action( 'init', 'remove_wpml_features' );
 function remove_wpml_features() {
 
 	global $sitepress;
 
-	// Remove WPML styles
+	if ( empty( $sitepress ) ) { return; }
+
+	// Remove WPML's generator meta tag
+	if ( ! empty ( $GLOBALS['sitepress'] ) ) {
+		add_action( 'wp_head', function() {
+			remove_action( current_filter(), array( $GLOBALS['sitepress'], 'meta_generator_tag' ) );
+		}, 0 );
+	}
+
+	// Remove stylesheets for WPML's Language Selector
 	define( 'ICL_DONT_LOAD_LANGUAGE_SELECTOR_CSS', true );
 
-	// Silence is golden
+	// Remove WPML promotions for translation services
 	define( 'ICL_DONT_PROMOTE', true );
+
+	// Hide iCanLocalize reminders panel
 	remove_action( 'admin_notices', array( $sitepress, 'icl_reminders' ) );
 
-	// Removes WPML Dashboard metabox
+	// Removes WPML's Dashboard metabox
 	remove_action( 'wp_dashboard_setup', array( $sitepress, 'dashboard_widget_setup' ) );
 
-}
-
-/**
- * Remove WPML's translation queue from subscribers
- */
-
-add_action( 'admin_menu', 'remove_wpml_admin_menu_entries', 9999 );
-function remove_wpml_admin_menu_entries() {
-	if ( ! current_user_can( 'edit_posts' ) ) {
-		remove_menu_page( 'wpml-translation-management/menu/translations-queue.php' );
+	// Remove Access Group and Content Template (requires Access plugin)
+	// Remove Access Group and Content Template (requires Access plugin)
+	if ( defined('Access_Helper') ) {
+		remove_action( 'admin_head', array( Access_Helper, 'wpcf_access_select_group_metabox' ));
 	}
+
 }
 
 /**
  * Remove WPML's Multilingual Content Setup metabox
- *
  * @link http://codex.wordpress.org/Function_Reference/remove_meta_box
  */
-
-add_action( 'admin_head', 'remove_wpml_metaboxes' );
-function remove_wpml_metaboxes() {
-	$screen = get_current_screen();
-	remove_meta_box( 'icl_div_config', $screen->post_type, 'normal' );
-}
+	add_action( 'admin_head', 'remove_wpml_metaboxes' );
+	function remove_wpml_metaboxes() {
+		$screen = get_current_screen();
+		remove_meta_box( 'icl_div_config', $screen->post_type, 'normal' );
+	}
